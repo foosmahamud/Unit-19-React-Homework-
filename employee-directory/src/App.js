90ea7e15b863fed29118a282f45431c7bf307411
Foos Mahamud <foosmahamud55@gmail.com>
2020-11-23 23:15:22 -0600

    updated components,untiles,employeeDirectory

---
+++ employee-directory/src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import EmployeeDirectory from "./components/Employee-directory";
+
+const App = () => <EmployeeDirectory/>;
+
+export default App;
---
+++ employee-directory/src/components/Employee-directory.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class EmployeeDirectory extends Component {
+  state = {
+    currentPage: "Home"
+  };
+
+//   handlePageChange = page => {
+//     this.setState({ currentPage: page });
+//   };
+
+  render() {
+    return (
+      <div>
+    <h1>Hello world</h1>
+      </div>
+    );
+  }
+}
+
+export default EmployeeDirectory;
